0c3c7856e5c689946144967030e8d414a72fd7f2
tomgdow <thomasgdowling@gmail.com>
2016-12-20 12:07:18 +0000

    fix show from  in template2

---
+++ public/javascripts/angular/ng_directives.js
@@ -0,0 +1,18 @@
+'use strict';
+/* Directives */
+angular.module('myApp.directives', []).
+directive('appVersion', ['version', function(version) {
+  return function(scope, elm, attrs) {
+    elm.text(version);
+  };
+}])
+.directive('productTable', function () {
+  return {
+    scope: {collection: '=',
+      'sendId': '&onSend'}, 
+      restrict: 'E',
+      templateUrl: function (elem, attr) {
+        return './html/' + attr.template + '.html'
+      } 
+  }
+});
